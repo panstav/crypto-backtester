6abd24d81b687c8a1375cf2382f2110cbd7db8d3
Stav Geffen <stavgeffen@gmail.com>
2021-11-24 20:37:34 +0200

    feat(backup)

---
+++ bin/backup-data.js
@@ -0,0 +1,22 @@
+import fs from 'fs-extra';
+
+import defaults from '../lib/defaults.js';
+
+(async ({ intervals }) => {
+
+	const filesAtDataDir = await fs.readdir('./data');
+
+	const filesByInterval = splitByInterval(filesAtDataDir, intervals);
+
+	await Promise.all(intervals.reduce((accu, interval) => accu.concat(filesByInterval[interval]
+		.map((fileName) => fs.copy(`./data/${fileName}`, `./historical-data/${interval}/${fileName}`))), []));
+
+})(defaults());
+
+function splitByInterval(filenames, intervals) {
+	return filenames.reduce((accu, fileName) => {
+		const key = intervals.find((interval) => fileName.includes(interval));
+		accu[key] = [...(accu[key] || []), fileName];
+		return accu;
+	}, {});
+}
